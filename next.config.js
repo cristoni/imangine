c4568b52873b1be3e4746269f9065895ab5a3dd6
Gabriele Cristoni <gabrielecristoni@gmail.com>
2018-11-30 00:42:31 +0100

    - added next-compose-plugins; - changed the import of purgeCss to only NODE_ENV production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-const withSass = require("@zeit/next-sass");
-const withPurgeCss = require("next-purgecss");
+const withPlugins = require('next-compose-plugins');
+const plugins = [];
 
-module.exports = withSass(withPurgeCss({
-  purgeCss: {
-    whitelist: () => ['body']
-  }
-}));
+plugins.push( require("@zeit/next-sass") );
+
+if (process.env.NODE_ENV === 'production') {
+  const purgeOptions = {
+    purgeCss: {
+      whitelist: () => ['body']
+    }
+  };
+
+  plugins.push( [require("next-purgecss"), purgeOptions] );
+}
+
+module.exports = withPlugins( plugins );
