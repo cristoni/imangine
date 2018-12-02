4023d5173bf5f5bc4f0dda90b51fdfcce3dd94cf
Gabriele Cristoni <gabrielecristoni@gmail.com>
2018-12-02 22:59:06 +0100

    - remove backdrop

--- components/side-modal.js
+++ components/side-modal.js
@@ -1,17 +1,12 @@
 import { Spring } from 'react-spring'
 
-const handleContentClick = (e) => {
-  e.preventDefault()
-  e.stopPropagation()
-}
-
 const SideModal = ({open, close, children}) => (
-  <div className={`side-modal ${open ? 'side-modal--open' : ''}`} onClick={close}>
+  <div className={`side-modal ${open ? 'side-modal--open' : ''}`}>
     {!!open && <Spring
       from={{ opacity: 0.8, transform: 'translate3d(-100%,0,0)' }}
       to={{ opacity: 1, transform: 'translate3d(0%,0,0)' }}
     >
-      {props => <div style={props} className="side-modal__content shadow" onClick={handleContentClick}>
+      {props => <div style={props} className="side-modal__content shadow">
         <div className="side-modal__clear" onClick={close}>
           <i className="ml-2 material-icons">clear</i>
         </div>
